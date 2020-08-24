a8ed412ef018542b4d60f11df54e5a34bd77f5c3
Darius Lee Pasilaban <p.dariuslee@gmail.com>
2020-08-24 16:38:21 -0700

    scaffolding files

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// ============ packages ============
+
+const express = require('express');
+// const superagent = require('superagent');
+
+
+
+// ============ global vars ============
+
+const PORT = process.env.PORT || 3003;
+const app = express();
+
+
+// ============ express configs ============
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
+app.use(express.urlencoded({extended: true}));
+
+app.get('/hello', (req,res) => {
+  res.render('pages/index');
+})
+
+
+// ============ start server ============
+
+
+app.listen(PORT, () => console.log(`we are running on PORT : ${PORT}`));
